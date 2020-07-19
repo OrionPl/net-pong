a080b246005562ecf0fa2d68a5f996a3986ed41c
Mikołaj Kierski <mikolaj.kierski@outlook.com>
2020-07-19 21:17:22 +0200

    QuikFix - font causing client to crash

--- Client/command_o.sh
+++ Client/command_o.sh
@@ -1,1 +1,1 @@
-g++.exe src/main.cpp src/Window.cpp src/Ball.cpp src/Paddles.cpp src/Score.cpp src/Networking.cpp src/Utilities/Helper.cpp -I"E:\include\SDL2\include" -L"E:\include\SDL2\lib\x64" -w -Wl,-subsystem,windows -lmingw32 -lSDL2main -lSDL2 -lSDL2_ttf -lws2_32 -o bin/main.exe && echo "-" && ./bin/main.exe
+g++.exe src/main.cpp src/Window.cpp src/Ball.cpp src/Paddles.cpp src/Score.cpp src/Networking.cpp src/Utilities/Helper.cpp -I"E:\include\SDL\64\include" -L"E:\include\SDL\64\lib" -w -Wl,-subsystem,windows -lmingw32 -lSDL2main -lSDL2 -lSDL2_ttf -lws2_32 -o bin/main.exe
--- Server/command_o.sh
+++ Server/command_o.sh
@@ -1,1 +1,1 @@
-g++ main.cpp Networking.cpp User.cpp Server.cpp Utilities/Helper.cpp -lws2_32
+g++ src/main.cpp src/Networking.cpp src/User.cpp src/Server.cpp src/Logic.cpp src/Utilities/Helper.cpp -lws2_32 -o bin/main.exe
