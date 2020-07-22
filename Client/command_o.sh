932b692936dd0d87cf29b7e752dfbec1011fc9b1
Mikołaj Kierski <mikolaj.kierski@outlook.com>
2020-07-22 18:07:07 +0200

    .gitignore quik update

--- Client/command_o.sh
+++
@@ -1,1 +0,0 @@
-g++.exe src/main.cpp src/Window.cpp src/Ball.cpp src/Paddles.cpp src/Score.cpp src/Networking.cpp src/Utilities/Helper.cpp -I"E:\include\SDL\64\include" -L"E:\include\SDL\64\lib" -w -Wl,-subsystem,windows -lmingw32 -lSDL2main -lSDL2 -lSDL2_ttf -lws2_32 -o bin/main.exe
