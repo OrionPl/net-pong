e3d985f5533ddc7ae075f7f3293b93f93674b9e8
Stan O <spammyboy@protonmail.com>
2020-08-03 14:56:10 +0200

    settings properly loading from file

--- Server/src/main.cpp
+++ Server/src/main.cpp
@@ -1,18 +1,18 @@
 #pragma once
 
-#include <map>
-
 #include "Utilities/Print.h"
 
-#include "SettingsControl.hpp"
+#include "Settings.hpp"
 #include "Networking.hpp"
 #include "Server.hpp"
 
 #include "Logic.hpp"
 
 int main(int argc, char*[]) {
-    SettingsControl settings_control;
-    settings_control.LoadSettings();
+
+    Settings settings;
+    LoadSettings(&settings);
+
     Logic logic;
     Networking net(55555, &logic);
     Server* serv = net.GetServer();
